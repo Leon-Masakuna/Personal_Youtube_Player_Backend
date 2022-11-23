0ab40241a7c6f6961bbc7e486261c10cc8f418fd
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-11-23 15:54:15 +0100

    feat : comments routes

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const commentCtrl = require("../controllers/comments");
 
-router.post("/", commentCtrl.createComment)
-router.get("/", commentCtrl.getAllComment)
+router.post("/", commentCtrl.createComment);
+router.get("/", commentCtrl.getAllComment);
+router.get("/:id", commentCtrl.getOneComment);
+router.put("/:id", commentCtrl.addSubComment);
 
 module.exports = router;
