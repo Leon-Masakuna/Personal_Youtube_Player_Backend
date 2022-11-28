8fc8a7e103143506a695d912e2e03fafea8be604
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-11-28 12:19:39 +0100

    feat : user model

--- models/users.js
+++ models/users.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastname: {type: String, required: true},
+    names: {type: String, required: true},
     email: {type: String, required: true},
     avatar: {type: String, required: true},
     linkedin: {type: String},
     github: {type: String},
-    comments: {type: Array}
 });
 
 module.exports = mongoose.model("users", userSchema);
