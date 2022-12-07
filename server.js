858f28f85c0e8d96f5b71ea3c3b254d8ee2bb7c7
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-07 20:01:44 +0100

    feat : app server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const commentRoute = require("./routes/comments")
-const userRoute = require("./routes/users")
+const commentRoute = require("./routes/comments");
+const userRoute = require("./routes/users");
 
-const port = process.env.PORT  || 8100
+const port = process.env.PORT || 8100;
 
 mongoose
   .connect(
@@ -19,10 +19,22 @@ mongoose
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/comment", commentRoute);
 app.use("/api/user", userRoute);
 
 app.listen(port, () => {
-    console.log(`The application is running on the port ${port}`);
-})
-
+  console.log(`The application is running on the port ${port}`);
+});
