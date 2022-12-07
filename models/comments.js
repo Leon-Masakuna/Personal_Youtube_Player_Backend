efac9ccb207640856a472994977d533ba854366f
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-07 19:49:57 +0100

    feat : comment model

--- models/comments.js
+++ models/comments.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const commentSchema = mongoose.Schema({
-    message: {type: String, required: true},
-    time: {type: String},
-    video: {type: String},
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Users",
-        required: true
-    },
-    subComments : [{type: Array}]
-})
+  message: { type: String, required: true },
+  time: { type: String },
+  video: { type: String },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: false,
+  },
+  subComments: [{ type: Array }],
+});
 
 module.exports = mongoose.model("Comments", commentSchema);
