f1faffca0bfd9792dfd0f7cfd5082822b35f733b
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-09 16:44:51 +0100

    feat : like controller

--- controllers/likes.js
+++ controllers/likes.js
@@ -3,7 +3,6 @@ const Like = require("../models/like");
 exports.createLike = (req, res, next) => {
   const like = new Like({
     commentId: req.body.commentId,
-    videoId: req.body.videoId,
     userId: req.body.userId,
   });
   like
@@ -24,6 +23,20 @@ exports.getAllLike = (req, res, next) => {
   Like.find()
     .then((likes) => {
       res.status(200).json(likes);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.deleteLike = (req, res, next) => {
+  Like.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Like Mention Deleted Successfully!",
+      });
     })
     .catch((error) => {
       res.status(400).json({
